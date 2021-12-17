f1497358234b8c009d659ad869433ad32841cbca
vanderson nogueira <vanderson.01@hotmail.com>
2021-12-17 19:01:31 -0300

    Adicionando o serviço de categorias

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, flatMap } from 'rxjs/operators'
+import { Category } from '../../shared/category.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiPath: string = "api/categories";
+
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Category[]> {
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategories)
+    )
+  }
+
+  getbyId(id: number): Observable<Category> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(map(this.jsonToCategory));
+  }
+
+
+   create(category: Category) : Observable<Category>{
+
+    return this.http.post(this.apiPath, category).pipe(
+      map(this.jsonToCategory)
+    );
+   }
+
+  update(category: Category): Observable<Category> {
+    const url = `${this.apiPath}/${category.id}`;
+
+
+    return this.http.put(url ,  category).pipe(
+      map(() => category)
+      // map(this.jsonToCategory)
+    );
+  }
+
+
+  jsonToCategory(category: any): Category {
+    //apenas um cast
+    return category as Category;
+  }
+
+
+
+  handleError(e: any): Observable<any> {
+    console.log("Erro na requisição => ", e);
+    return throwError(e);
+  }
+
+  private jsonDataToCategories(json: any[]): Category[] {
+    const categories: Category[] = [];
+    json.forEach(e => categories.push(e as Category))
+    return categories;
+  }
+}
